87c970c8e7d0c589f2d3b42f4fcc0036a0ea372e
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-07-04 20:55:39 -0300

    HighlightedTitle criado

--- src/components/TituloDestacado/HighlightedTitle.jsx
+++ src/components/TituloDestacado/HighlightedTitle.jsx
@@ -5,7 +5,7 @@ import ButtonLayout from '../ButtonLayout/ButtonLayout';
 const HighlightedTitle = () => {
   return (
     <div className="highlighted-title">
-      <h1 className="h1-highlighted">Transformando ideias em realidade digital</h1>
+      <h1 className="h1-highlighted">Transformando <span className="h1-highlighted-destaque">ideias</span><br/>em realidade digital</h1>
       <h2 className="h2-highlighted">Desenvolvimento web personalizado utilizando React, Node.js e muito mais que há de mais moderno digitalmente</h2>
       <ButtonLayout textoBotao="Começar" />
     </div>
