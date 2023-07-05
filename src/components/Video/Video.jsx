2d78e641f364b176b081e0e67bbabac1c591558c
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-07-04 21:00:15 -0300

    video estilizado

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './Video.css';
 import video from '../../assets/videos/video-featured.mp4';
 
+
 const Video = () => {
   return (
     <div className="video">
-      <video src={video} autoPlay loop muted className="video" />
+      <video autoPlay loop muted className="video-background">
+        <source src={video} type="video/mp4" />
+        Seu navegador não suporta vídeos em HTML5.
+      </video>
     </div>
   );
 };
