6ff8e01412aa65ce37959e60e900bb0d09c26037
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-07-18 12:26:10 -0300

    removido imports inexistentes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import wppsvg from '../../assets/images/wpp.svg';
 import instasvg from '../../assets/images/instagram.svg';
-import ifoodpng from '../../assets/images/ifood-branco.png';
 import defaultImage from '../../assets/images/defaultImage.png';
 import Link from 'next/link';
 
@@ -37,9 +36,7 @@ const Carousel = ({ images, interval }) => {
             <Link href="https://wa.me/5531991525735?text=Ol%C3%A1%20Glayde,%20te%20encontrei%20atrav%C3%A9s%20do%20seu%20site!" target="_blank" rel="noreferrer" title="link whatsapp glayde ribeiro">
               <Image src={wppsvg} alt="whatsapp botao" className="svgbtn svg__wpp" />
             </Link>
-            <Link href="https://www.ifood.com.br/delivery/belo-horizonte-mg/confeitaria-glayde-ribeiro-doces--bolos-salgado-filho/0ccfe710-ac48-44f0-a2e6-86e298d060ad?utm_medium=share" target="_blank" rel="noreferrer" title="link ifood glayde ribeiro">
-              <Image src={ifoodpng} alt="ifood botao" className="svgbtn svg__ifood" />
-            </Link>
+
           </div>
           <button className="carousel__nav-button" onClick={handlePrevClick}>&lt;</button>
           <button className="carousel__nav-button" onClick={handleNextClick}>&gt;</button>
@@ -69,9 +66,6 @@ const Carousel = ({ images, interval }) => {
           </Link>
           <Link href="https://wa.me/5531991525735?text=Ol%C3%A1%20Glayde,%20te%20encontrei%20atrav%C3%A9s%20do%20seu%20site!" title="link whatsapp glayde ribeiro" target="_blank" rel="noreferrer">
             <Image src={wppsvg} alt="wpp icon" className="svgbtn svg__wpp" />
-          </Link>
-          <Link href="https://www.ifood.com.br/delivery/belo-horizonte-mg/confeitaria-glayde-ribeiro-doces--bolos-salgado-filho/0ccfe710-ac48-44f0-a2e6-86e298d060ad?utm_medium=share" target="_blank" rel="noreferrer" title="link ifood glayderibeiro">
-            <Image src={ifoodpng} alt="ifood icon" className="svgbtn svg__ifood" />
           </Link>
         </div>
 
