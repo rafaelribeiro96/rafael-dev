2d6a5124bedd6922645a4d1161f24bcedcedccdd
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-07-18 23:05:35 -0300

    adicionado link no card inteiro do projeto

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 
 const Card = ({ image, title, description, link }) => {
   return (
-    <div className={styles.card}>
+      <Link href={link} className={styles.card} target="_blank" rel="noreferrer">
       <Image src={image} alt={title} className={styles.card_image} />
       <h2 className={styles.card_title}>{title}</h2>
       <p className={styles.card_description}>{description}</p>
-      <Link href={link} className={styles.card_link} target="_blank" rel="noreferrer">
+      <span href={link} className={styles.card_link} target="_blank" rel="noreferrer">
         Ver mais
+      </span>
       </Link>
-    </div>
   );
 };
 
