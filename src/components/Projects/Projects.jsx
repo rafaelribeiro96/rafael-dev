996000453205f5f11e0671b4f876f88ead7497f3
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-10-06 10:11:32 -0300

    Adicionado projeto Granola Predileta

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Projects.module.css';
 import Card from './Card';
 import image1 from '../../assets/images/modelo-site.png';
-import image2 from '../../assets/images/portfolio-rafael.png';
-import image3 from '../../assets/images/pokedex.png';
+import image2 from '../../assets/images/granola.png';
+import image3 from '../../assets/images/portfolio-rafael.png';
 
 const Projects = () => {
   return (
@@ -19,15 +19,21 @@ const Projects = () => {
         />
         <Card
           image={image2}
-          title="Portfólio Pessoal Rafael Ribeiro"
-          description="Desenvolvemos um portfólio pessoal para o Rafael Ribeiro, exibindo seu histórico profissional e acadêmico de maneira atrativa. Confira como criamos uma presença online que reflete a essência da pessoa."
-          link="https://rafaelribeiro96.github.io/"
+          title="Granola Predileta"
+          description="Desenvolvemos um site para a Granola Predileta, exibindo seus produtos de maneira atrativa. Confira como criamos seu site de forma responsiva e atraente, com uma boa usabilidade."
+          link="https://granolapredileta.vercel.app/"
         />
-        <Card
-          image={image3}
+        {/* <Card
+          image={image2}
           title="Pokedex"
           description="Desenvolvemos uma pokedex, exibindo os Pokemons de maneira atrativa e dinâmica. Confira como criamos uma aplicação com design responsivo e com uma boa usabilidade."
           link="https://pokedexrr.netlify.app/"
+        /> */}
+        <Card
+          image={image3}
+          title="Portfólio Pessoal Rafael Ribeiro"
+          description="Desenvolvemos um portfólio pessoal para o Rafael Ribeiro, exibindo seu histórico profissional e acadêmico de maneira atrativa. Confira como criamos uma presença online que reflete a essência da pessoa."
+          link="https://rafaelribeiro96.github.io/"
         />
       </div>
     </div>
